8863b3b7a9b161a66a547cff7cb135d5fa8b7805
moritzantes30 <moritz.antes@t-online.de>
2026-02-13 14:04:59 +0100

    Hinzugefügt

---
+++ data.js
@@ -0,0 +1,856 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-04-treppenhaus",
+      "name": "04 Treppenhaus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.23605931184897777,
+        "pitch": 0.8258249231329753,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21403071172683674,
+          "pitch": 0.11038438350714941,
+          "rotation": 6.283185307179586,
+          "target": "7-04-wohnung-02-flur-01"
+        },
+        {
+          "yaw": -1.7652292612025704,
+          "pitch": 0.123807253939475,
+          "rotation": 0,
+          "target": "1-04-wohnung-01-flur-01"
+        },
+        {
+          "yaw": 1.4216515886608008,
+          "pitch": 0.09583504118026909,
+          "rotation": 0,
+          "target": "12-04-wohnung-03-flur-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-04-wohnung-01-flur-01",
+      "name": "04 Wohnung 01 Flur 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.005370243852290457,
+          "pitch": 0.0827093003777577,
+          "rotation": 0,
+          "target": "2-04-wohnung-01-flur02"
+        },
+        {
+          "yaw": -3.0957049271400674,
+          "pitch": 0.11903600029192063,
+          "rotation": 0,
+          "target": "0-04-treppenhaus"
+        },
+        {
+          "yaw": -1.7002450158515252,
+          "pitch": 0.12549059340187618,
+          "rotation": 0,
+          "target": "5-04-wohnung-01-schlafzimmer-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-04-wohnung-01-flur02",
+      "name": "04 Wohnung 01 Flur02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7821400307694581,
+          "pitch": 0.03671944660670157,
+          "rotation": 0,
+          "target": "4-04-wohnung-01-kinderzimmer-02"
+        },
+        {
+          "yaw": -1.3105050169090529,
+          "pitch": 0.06065177389298171,
+          "rotation": 0,
+          "target": "3-04-wohnung-01-kinderzimmer-01"
+        },
+        {
+          "yaw": 1.2017970057321978,
+          "pitch": 0.06756535942152198,
+          "rotation": 0,
+          "target": "6-04-wohnung-01-kochen-essen-wohnen"
+        },
+        {
+          "yaw": -3.095748398966201,
+          "pitch": 0.11234308240773849,
+          "rotation": 0,
+          "target": "1-04-wohnung-01-flur-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04-wohnung-01-kinderzimmer-01",
+      "name": "04 Wohnung 01 Kinderzimmer 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0265858984939946,
+          "pitch": 0.11763666925282834,
+          "rotation": 0,
+          "target": "2-04-wohnung-01-flur02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-04-wohnung-01-kinderzimmer-02",
+      "name": "04 Wohnung 01 Kinderzimmer 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1241859944480126,
+          "pitch": 0.08231411340758044,
+          "rotation": 0,
+          "target": "2-04-wohnung-01-flur02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-04-wohnung-01-schlafzimmer-01",
+      "name": "04 Wohnung 01 Schlafzimmer 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0476983898900816,
+          "pitch": 0.020671016346113902,
+          "rotation": 0,
+          "target": "1-04-wohnung-01-flur-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-04-wohnung-01-kochen-essen-wohnen",
+      "name": "04 Wohnung 01 Kochen-Essen-Wohnen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9166414482507665,
+          "pitch": 0.1206807100525431,
+          "rotation": 0,
+          "target": "2-04-wohnung-01-flur02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-04-wohnung-02-flur-01",
+      "name": "04 Wohnung 02 Flur 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7218998536546195,
+          "pitch": 0.18666180438424895,
+          "rotation": 0,
+          "target": "8-04-wohnung-02-abstellraum-01"
+        },
+        {
+          "yaw": -0.5132175419987384,
+          "pitch": 0.16765413136759832,
+          "rotation": 5.497787143782138,
+          "target": "9-04-wohnung-02-schlafzimmer-01"
+        },
+        {
+          "yaw": 0.5231596229608382,
+          "pitch": 0.2409932781322155,
+          "rotation": 0,
+          "target": "10-04-wohnung-02-kochen-essen-wohnen-"
+        },
+        {
+          "yaw": 2.9501006871993427,
+          "pitch": 0.171803840441207,
+          "rotation": 0,
+          "target": "0-04-treppenhaus"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-04-wohnung-02-abstellraum-01",
+      "name": "04 Wohnung 02 Abstellraum 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5799459090256533,
+          "pitch": 0.2889790226479043,
+          "rotation": 0,
+          "target": "7-04-wohnung-02-flur-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-04-wohnung-02-schlafzimmer-01",
+      "name": "04 Wohnung 02 Schlafzimmer 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3837410872067224,
+          "pitch": 0.15829005909827387,
+          "rotation": 0,
+          "target": "7-04-wohnung-02-flur-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-04-wohnung-02-kochen-essen-wohnen-",
+      "name": "04 Wohnung 02 Kochen-Essen-Wohnen ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3294566801551007,
+          "pitch": 0.13510724650089045,
+          "rotation": 0,
+          "target": "9-04-wohnung-02-schlafzimmer-01"
+        },
+        {
+          "yaw": 0.6596828652446796,
+          "pitch": 0.14559054344302247,
+          "rotation": 0,
+          "target": "7-04-wohnung-02-flur-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-04-wohnung-02-bad-01",
+      "name": "04 Wohnung 02 Bad 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-04-wohnung-03-flur-01",
+      "name": "04 Wohnung 03 Flur 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4069423343542269,
+          "pitch": 0.12392976015927104,
+          "rotation": 0,
+          "target": "14-04-wohnung-03-bad-01"
+        },
+        {
+          "yaw": -0.043861616684349514,
+          "pitch": 0.08607871728288252,
+          "rotation": 0,
+          "target": "13-04-wohnung-03-flur-02"
+        },
+        {
+          "yaw": 3.0753296205932905,
+          "pitch": 0.008423241081555233,
+          "rotation": 0,
+          "target": "0-04-treppenhaus"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-04-wohnung-03-flur-02",
+      "name": "04 Wohnung 03 Flur 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.37200592413375233,
+          "pitch": 0.22404892630905238,
+          "rotation": 0,
+          "target": "16-04-wohnung-03-kinderzimmer-02"
+        },
+        {
+          "yaw": 0.6256312353285995,
+          "pitch": 0.17957187702620736,
+          "rotation": 0,
+          "target": "15-04-wohnung-03-kinderzimmer-01"
+        },
+        {
+          "yaw": -2.6034118960698134,
+          "pitch": 0.05936346502803502,
+          "rotation": 0,
+          "target": "17-04-wohnung-03-kochen-essen-wohnen"
+        },
+        {
+          "yaw": 1.743285365990177,
+          "pitch": 0.14563728577852508,
+          "rotation": 0,
+          "target": "12-04-wohnung-03-flur-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-04-wohnung-03-bad-01",
+      "name": "04 Wohnung 03 Bad 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6333711085644769,
+          "pitch": 0.12688828962702559,
+          "rotation": 0,
+          "target": "12-04-wohnung-03-flur-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-04-wohnung-03-kinderzimmer-01",
+      "name": "04 Wohnung 03 Kinderzimmer 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1121120579507497,
+          "pitch": 0.08909901995333769,
+          "rotation": 0,
+          "target": "13-04-wohnung-03-flur-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-04-wohnung-03-kinderzimmer-02",
+      "name": "04 Wohnung 03 Kinderzimmer 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9669816599366605,
+          "pitch": 0.044405273164228376,
+          "rotation": 0,
+          "target": "13-04-wohnung-03-flur-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-04-wohnung-03-kochen-essen-wohnen",
+      "name": "04 Wohnung 03 Kochen-Essen-Wohnen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9799028359531867,
+          "pitch": 0.10896761845386749,
+          "rotation": 0,
+          "target": "13-04-wohnung-03-flur-02"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "GAG SWS Haus C 2.OG",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
